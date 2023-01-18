66423404ce67b3e07ddc5e00035ecc37d5da0ed4
codecaiine <akabrouyannickn@gmail.com>
2023-01-18 20:18:41 +0000

    finish the slide

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -49,6 +49,30 @@ const Home = () => {
     <>
     <section className='py-20 lg:py-36 xl:py-48'>
 
+       <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+      ></div>
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prev} size={30} />
+      </div>
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={next} size={30} />
+      </div>
+      <div className='flex top-4 justify-center py-2'>
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className='text-2xl cursor-pointer'
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
+
       <div className='mt-10 lg:mx-10 gap-8  flex flex-col md:flex-row items-center'>
           <div className='md:flex-1 md:order-2'>
           
