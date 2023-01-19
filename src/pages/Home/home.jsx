5d836fe35c1115d320328068051bbcb5d4262233
codecaiine <akabrouyannickn@gmail.com>
2023-01-19 20:40:22 +0000

    Add images

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -13,20 +13,20 @@ const Home = () => {
 
   const carousel = [
     {
-      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+      url: images.healthcare,
     },
     {
-      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+      url: images.healthcare1,
     },
     {
-      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+      url: images.healthcare2,
     },
 
     {
-      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+      url: images.healthcare3,
     },
     {
-      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+      url: images.healthcare4,
     },
   ];
 
@@ -52,7 +52,7 @@ const Home = () => {
     <>
     <section className='py-20'>
 
-    <div className='max-w-[1400px] h-[780px] w-full m-auto relative group mb-6'>
+    <div className='max-w-[1400px] h-[780px] m-auto relative group mb-6'>
       <div
         style={{ backgroundImage: `url(${carousel[currentIndex].url})` }}
         className='w-full h-full bg-center bg-cover duration-500'
