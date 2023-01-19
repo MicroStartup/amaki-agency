865f4f637e82da52127a0ab519f1127dfde3f9f8
codecaiine <akabrouyannickn@gmail.com>
2023-01-19 21:58:27 +0000

    Add footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { FaFacebook, FaTwitter, FaWhatsapp} from 'react-icons/fa';
+import { images } from '../constants'
 
 const Footer = () => {
   return (
-         <footer className='grid grid-cols md:grid-cols-3
-              border-black border-t bg-white'>
-               <div  className='mx-20 my-5 md:col-start-3 flex gap-3 items-center justify-center text-lg'>
-               
-                 <Link to='/'>
+    
+    <div className='w-full mt-24 bg-slate-900 text-gray-300 py-y'>
+     <div className='grid-cols-2 gap-5 flex flex-wrap text-black bg-[#f8f9fa] py-12'>
+        <div className=' md:flex-1 
+          md:order-2 container flex-col lg:flex-row px-5'>
+          <img className='mx-auto' src={images.healthcare8} alt='Raddy'/>
+                
+       </div>
+        <div className='px-5 md:flex-1 
+          md:order-2 container flex-col lg:flex-row'>
+                <p className='font-bold uppercase'>Subscribe to our newsletter</p>
+                <p className='py-4'>Subscribe to our communications to receive our best offers!.</p>
+                <form className='flex flex-col sm:flex-row'>
+                    <input className='w-full p-2 mr-4 rounded-md mb-4' type="email" placeholder='Enter email..'/>
+                    <button className='p-2 mb-4 bg-blue-700 text-white uppercase'>Subscribe</button>
+                </form>
+        </div>
+    </div>
+
+    <div className='flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500'>
+    <p className='py-4'>Copyright &copy; 2023 Amaki-Agency</p>
+    <div className='flex justify-between sm:w-[300px] pt-4 text-2xl'>
+<Link to='/'>
                     <FaFacebook />
                  </Link>
                  <Link to='/'>
@@ -17,12 +36,9 @@ const Footer = () => {
                  <Link to='/'>
                    <FaTwitter/>
                  </Link>
-               </div>
-           <div className='col-span-2 md:row-start-1 md:col-start-2
-          md:col-end-3 border-t-2 border-black md:border-t-0 flex items-center
-          justify-center p-4 text-center'>Copyright &copy; 2023 Amaki-Agency
-          </div>
-       </footer>
+    </div>
+    </div>
+</div>
   )
 }
 
