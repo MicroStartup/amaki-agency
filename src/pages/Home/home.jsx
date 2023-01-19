5882de6e8a5c1973dcfac73bbd5514772edae2c7
codecaiine <akabrouyannickn@gmail.com>
2023-01-19 20:29:42 +0000

    Implement carousel

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,13 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import { images } from '../../constants';
 import { workData } from '../../data';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
+
+
+
 
 const Home = () => {
+
+  const carousel = [
+    {
+      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+    },
+
+    {
+      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prev = () => {
+    const firstImage = currentIndex === 0;
+    const newIndex = firstImage ? carousel.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const next = () => {
+    const lastImage = currentIndex === carousel.length - 1;
+    const newIndex = lastImage ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
     <>
-    <section className='py-20 lg:py-36 xl:py-48'>
+    <section className='py-20'>
+
+    <div className='max-w-[1400px] h-[780px] w-full m-auto relative group mb-6'>
+      <div
+        style={{ backgroundImage: `url(${carousel[currentIndex].url})` }}
+        className='w-full h-full bg-center bg-cover duration-500'
+      ></div>
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prev} size={30} />
+      </div>
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={next} size={30} />
+      </div>
+      <div className='flex top-4 justify-center py-2'>
+        {carousel.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className='text-2xl cursor-pointer'
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
 
       <div className='mt-10 lg:mx-10 gap-8  flex flex-col md:flex-row items-center'>
           <div className='md:flex-1 md:order-2'>
