39a501d3f208ca84753d8aa22fae62d294892f3a
OLIPLICHE <olipliche@gmail.com>
2023-01-21 00:14:10 +0100

    solve baggs

--- src/pages/Services/services.jsx
+++ src/pages/Services/services.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
-import { images } from '../../constants';
 import { serviceData } from '../../serviceData';
 import ServiceItem from './ServiceItem';
 
